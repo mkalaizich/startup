d2750998e325726499f6721867a3371de3bec46e
NicolasDF <ndelfiorentino@gmail.com>
2016-09-17 16:33:47 -0300

    [Nicolas Del Fiorentino] - Exercise 6 - Done

--- 01-JS-DOM-APIs/script.js
+++ 01-JS-DOM-APIs/script.js
@@ -11,12 +11,31 @@ function showHidden () {
     }
 }
 
+function getJoke () {
+	var request = new XMLHttpRequest();
+	request.open('GET', 'http://api.icndb.com/jokes/random', true);
+
+	request.onload = function() {
+	  if (this.status >= 200 && this.status < 400) {
+	    var data = JSON.parse(this.response);
+	    var newP = document.createElement('p');
+	    newP.className = 'joke';
+	    document.getElementById('section_joke').appendChild(newP);
+	    newP.innerHTML = data.value.joke;
+	  } else {
+	    console.log('Server Error');
+
+	  }
+	};
+
+	request.onerror = function() {
+	  console.log('Connection error');
+	};
+
+	request.send();
+}
+
 document.addEventListener('DOMContentLoaded', function() { 
 	showHidden();
 	document.getElementById("btn_joke").addEventListener("click", getJoke);
 });
-
-
-
-
-
