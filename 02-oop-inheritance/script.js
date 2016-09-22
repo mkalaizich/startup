929b80552e2dae0ec9281ef17ff4b57c4e86a495
Nicolas <ndelfiorentino@gmail.com>
2016-09-22 14:28:44 -0300

    [Nicolas Del Fiorentino] - Exercise 6 - Keypoint done.

--- 02-oop-inheritance/script.js
+++ 02-oop-inheritance/script.js
@@ -1,10 +1,11 @@
 let Social = {
     share : function (friendName) {
-        //Object.assign(Social, Movie);
-        console.log(`Share ${Movie.title} with ${friendName}`);
+        Object.assign(this, Movie);
+        console.log(`Share ${this.title} with ${friendName}`);
     },
     like : function (friendName) {
-
+        Object.assign(this, Movie);
+        console.log(`${friendName} liked ${this.title}`);
     }
 };
 
@@ -13,8 +14,8 @@ class EventEmitter {
         this.events = { };        
     }
 
-    on (event, callback) {        
-        this.events[event] = callback;        
+    on (event, callback) {
+        this.events[event] = callback;
     }
     emit (event) {
         if (this.events[event]) {
@@ -31,14 +32,14 @@ class EventEmitter {
 class Movie extends EventEmitter {
     constructor (title, year, duration) {
         super();
-        //Object.assign(Movie, Social);
+        Object.assign(this, Social);
         this.title = title;
         this.year = year;
         this.duration = duration;
     }
 
-    play () {               
-        super.emit('play');       
+    play () {
+        super.emit('play');
     }
     pause () {
         super.emit('pause')
@@ -57,7 +58,8 @@ class Logger {
 document.addEventListener('DOMContentLoaded', function() { 
     let lotr = new Movie('Lord of the rings', 2002, '228 min');
     let logger = new Logger();
-    lotr.on('play', logger.log);    
+    lotr.on('play', logger.log);
     lotr.play(); 
     lotr.share('Mike Blossom');
+    lotr.like('Marco Aurelio');
 });
