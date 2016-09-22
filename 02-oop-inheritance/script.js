69cad7b91cbed847330c569f85278a2651c3678a
Nicolas <ndelfiorentino@gmail.com>
2016-09-22 13:21:32 -0300

    [Nicolas Del Fiorentino] - Exercise 6 Pending and stuck - Added Template Literals

--- 02-oop-inheritance/script.js
+++ 02-oop-inheritance/script.js
@@ -1,26 +1,27 @@
-var Social = {
-    share : function(friendName) {
-
+let Social = {
+    share : function (friendName) {
+        //Object.assign(Social, Movie);
+        console.log(`Share ${Movie.title} with ${friendName}`);
     },
-    like : function(friendName) {
+    like : function (friendName) {
 
     }
 };
 
 class EventEmitter {
-    constructor() {
-        this.events = { } ;
+    constructor () {
+        this.events = { };        
     }
 
-    on(event, callback) {        
+    on (event, callback) {        
         this.events[event] = callback;        
     }
-    emit(event) {
+    emit (event) {
         if (this.events[event]) {
             this.events[event](event);
         }
     }
-    off(event, callback) {
+    off (event, callback) {
         if (this.events[event]) {
             delete this.events[event];
         }
@@ -28,26 +29,27 @@ class EventEmitter {
 }
 
 class Movie extends EventEmitter {
-    constructor(title, year, duration) {
+    constructor (title, year, duration) {
         super();
+        //Object.assign(Movie, Social);
         this.title = title;
         this.year = year;
         this.duration = duration;
     }
 
-    play() {               
+    play () {               
         super.emit('play');       
     }
-    pause() {
+    pause () {
         super.emit('pause')
     }
-    resume() {
+    resume () {
         super.emit('resume');
     }
 }
 
 class Logger {
-    log(event) {
+    log (event) {
         console.log('The '+event+' event has been emitted!');
     }
 }
@@ -55,9 +57,7 @@ class Logger {
 document.addEventListener('DOMContentLoaded', function() { 
     let lotr = new Movie('Lord of the rings', 2002, '228 min');
     let logger = new Logger();
-    lotr.on('play', logger.log);
-    lotr.on('pause', logger.log);
-    lotr.off('pause', logger.log);
+    lotr.on('play', logger.log);    
     lotr.play(); 
-    lotr.pause(); 
+    lotr.share('Mike Blossom');
 });
