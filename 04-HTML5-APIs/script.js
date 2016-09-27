ccc2297093b611c738c0f1a870d3599ac9779a33
Nicolas <ndelfiorentino@gmail.com>
2016-09-27 11:52:37 -0300

    [Nicolas Del Fiorentino] - Exercise 1 and 2 - Keypoint done.

--- 04-HTML5-APIs/script.js
+++ 04-HTML5-APIs/script.js
@@ -1,14 +1,41 @@
 function saveContent () {
     if (typeof(Storage) !== "undefined") {
-        var textToSave = document.getElementById('text_area').value;
-        var target = document.getElementById('target_p');
-        console.log(Storage);
+        let textToSave = document.getElementById('text_area').value;
+        let target = document.getElementById('target_p');
         localStorage.setItem('text', textToSave);
         target.innerHTML = localStorage.getItem('text');
 
     } else {
         alert('Browser not supported.');
     }
+}
+
+function saveContentIndexedDB () {
+    let requestDB = indexedDB.open('text');
+
+    requestDB.onupgradeneeded = function () {
+        var database = requestDB.result;
+        let store = database.createObjectStore('string', {keyPath: "id"});
+    };
+
+    requestDB.onsuccess = function () {
+        database = requestDB.result;        
+        let transaction = database.transaction('string', 'readwrite');
+        let store = transaction.objectStore('string');
+        let textToSave = document.getElementById('text_area').value;
+
+        store.put({id: 1, info: textToSave});
+
+        transaction.oncomplete = function () {
+            console.log("Transaction completed succesfully");
+        };
+
+        database.close();
+    };
+
+    requestDB.onclose = function () {
+        console.log("database closed");
+    };
 }
 
 function eraseContent () {
@@ -18,6 +45,6 @@ function eraseContent () {
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('button_save').addEventListener('click', saveContent);
-    document.getElementById('button_clear').addEventListener('click', eraseContent);
-    
+    document.getElementById('button_save').addEventListener('click', saveContentIndexedDB);
+    document.getElementById('button_clear').addEventListener('click', eraseContent);    
 });
