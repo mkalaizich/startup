91c7a1ae1e057bae8a807aa9b7c3c9db85335b62
mkalaizich <mkalaizich@gmail.com>
2016-09-28 18:19:20 -0300

    [Martin Kalaizich] - Exercise 4 - Key point done. Also deleted unnecessary lines in setup.js script from exercise 3

---
+++ 04-HTML5-APIs/scripts/socket.js
@@ -0,0 +1,34 @@
+let websocket = new WebSocket("ws://echo.websocket.org/");
+
+document.addEventListener ('DOMContentLoaded', function () {
+
+    websocket.onopen = function(evt) { onOpen(evt) };
+    websocket.onmessage = function(evt) { onMessage(evt) };
+    document.querySelector('.sendbutton').addEventListener('click', send);
+    document.querySelector('.closebutton').addEventListener('click', close);
+});
+
+function onOpen(evt) {
+
+    console.log("CONNECTED");
+}
+
+function close () {
+
+    console.log("DISCONNECTED");
+    websocket.close()
+}
+
+function onMessage(evt) {
+
+    console.log('RESPONSE:' + evt.data);
+}
+
+function send () {
+
+    let message = document.querySelector(".messageinput").value;
+    
+    console.log("SENT:" + message);
+    websocket.send(message);
+    document.querySelector(".messageinput").value= '';
+}
