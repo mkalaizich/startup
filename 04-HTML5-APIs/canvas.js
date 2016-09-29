edfc1f9028ee7ae87816e0571b3d553bbfa18bbb
Nicolas <ndelfiorentino@gmail.com>
2016-09-29 10:03:59 -0300

    [Nicolas Del Fiorentino] - Exercise 5 - Figures now generate randomly

--- 04-HTML5-APIs/canvas.js
+++ 04-HTML5-APIs/canvas.js
@@ -1,47 +1,57 @@
-function drawCircle (canvastx) {
+function drawCircle (canvastx, circle) {
     canvastx.beginPath();
-    canvastx.arc(100,50,40,0,2*Math.PI);
-    canvastx.stroke();   
+    canvastx.arc(400, 100, circle.radius, 0, 2*Math.PI);
+    canvastx.stroke();
+    canvastx.fillStyle = circle.color;
+    canvastx.fill();
 }
 
-function drawRectangle (canvastx , rectangle) {
+function drawRectangle (canvastx, rectangle) {
     canvastx.beginPath();
-    canvastx.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
-    
+    canvastx.rect(20, 20, rectangle.width, rectangle.height);
+    canvastx.fillStyle = rectangle.color;
+    canvastx.fill();
     canvastx.stroke();
 }
 
-
-function drawSquare (canvastx) {
-    canvastx.rect(10,200,200,200);
+function drawSquare (canvastx, square) {
+    canvastx.beginPath();
+    canvastx.rect(800, 20, square.width, square.width);
+    canvastx.fillStyle = square.color;
+    canvastx.fill();
     canvastx.stroke();
 }
 
-function drawTriangle (canvastx) {
-    canvastx.beginPath();
-    canvastx.moveTo(400, 400);
-    canvastx.lineTo(550, 350);
-    canvastx.lineTo(400, 300);
-    canvastx.closePath();
-    canvastx.stroke();
+function randomNumber (min, max) {
+    return Math.floor(Math.random()* (max - min + 1) + min);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     let canvas = document.getElementById('canvas');
     let canvastx = canvas.getContext('2d');
-    let rectangle = {
-        x: 200,
-        y: 10,
-        width: 240,
-        height: 150
+    const colors = ['red', 'blue', 'green', 'cyan', 'magenta', 'white'];
+    let rectangle = {        
+        width: randomNumber(150, 300),
+        height: randomNumber(150, 300),
+        color: colors[randomNumber(0, colors.length)]
     };
+    let square = {
+        width: randomNumber(150, 300),
+        color: colors[randomNumber(0, colors.length)]
+    };
+    let circle = {
+        radius: randomNumber(80, 200),
+        color: colors[randomNumber(0, colors.length)]
+    }
+    console.log(circle.color);
+    console.log(square.color);
 
     for ( let i = 1 ; i <= 4; i++) {        
         canvastx.moveTo(100 * i, 10);
 
         switch (Math.floor((Math.random() * 3) + 1)) {
             case 1:
-                drawCircle(canvastx);
+                drawCircle(canvastx, circle);
 
                 break;
             case 2:
@@ -49,7 +59,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 break;
             case 3:
-                drawSquare(canvastx);
+                drawSquare(canvastx, square);
 
                 break;
         }
