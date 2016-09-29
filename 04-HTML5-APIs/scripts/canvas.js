f85dfc8fb14d8a7d0363411f4d82603c5f0a1edf
mkalaizich <mkalaizich@gmail.com>
2016-09-29 13:58:14 -0300

    [Martin Kalaizich] - Exercise 6 - Topic 4 Finished

--- 04-HTML5-APIs/scripts/canvas.js
+++ 04-HTML5-APIs/scripts/canvas.js
@@ -1,8 +1,14 @@
+let requestAnimationFrame = window.requestAnimationFrame || 
+                            window.mozRequestAnimationFrame || 
+                            window.webkitRequestAnimationFrame || 
+                            window.msRequestAnimationFrame;
+
 document.addEventListener ('DOMContentLoaded', function () {
 
     draw1 ();
     draw2 ();
     draw3 ();
+    animation ();
 });
 
 function draw1 () {
@@ -72,7 +78,7 @@ function draw2 () {
     grd.addColorStop(0,color);
     grd.addColorStop(1,"white");
     ctx.fillStyle = grd;
-    ctx.fillRect(x, y, x+base, y+height); 
+    ctx.fillRect(x, y, x + base, y + height); 
 }
 
 function draw3 () {
@@ -101,13 +107,44 @@ function draw3 () {
     }
 
     ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI*2 + 1);
+    ctx.arc(x, y, radius, 0, Math.PI * 2 + 1);
     ctx.fillStyle = color;
     ctx.strokeStyle = color;
     ctx.fill();
 }
 
+function animation () {
+
+    let mainCanvas = document.querySelectorAll(".frame--canvas")[3];
+    let mainContext = mainCanvas.getContext("2d");
+    let canvasWidth = mainCanvas.width;
+    let canvasHeight = mainCanvas.height;
+    let base = 40;
+    let height = 70;
+    let x = 0;
+    
+    function draw4 () {
+
+        let x1 = 2 + x;
+
+        mainContext.clearRect(0, 0, canvasWidth, canvasHeight);
+        mainContext.fillStyle = "white";
+        mainContext.fillRect(0, 0, canvasWidth, canvasHeight);
+        mainContext.fillStyle = "#006699";
+        mainContext.fillRect(x1, 0, base, height); 
+        x += 5;
+        if (x < canvasWidth - 40) {
+            requestAnimationFrame(draw4);
+        } else {
+            x = 0;
+            requestAnimationFrame(draw4);
+        }
+    }
+    draw4 ();
+}
+
+
 function randomInteger(min,max) {
     
-    return Math.floor(Math.random()*(max-min+1)+min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
