27a9f3a06a8b494f30e0182daac74da54648bace
Nicolas <ndelfiorentino@gmail.com>
2016-10-05 11:45:02 -0300

    [Nicolas Del Fiorentino] - Exercise 3 update - Routing not working.

--- 03-MVC-AngularJS/main.js
+++ 03-MVC-AngularJS/main.js
@@ -3,13 +3,16 @@ var movies = angular.module('movies', ['ngRoute']);
 /*movies.config(function ($routeProvider) {
     $routeProvider
         .when('/', {
-            templateUrl: './views/main.html'
+            templateUrl: 'views/main.html',
+            controller: 'MovieController'
         })
         .when('/addMovie', {
-            templateUrl: './views/addMovie.html'
+            templateUrl: 'views/addMovie.html',
+            controller: 'MovieController'
         })
         .when('/movies', {
-            templateUrl: './views/main.html'
+            templateUrl: 'views/main.html',
+            controller: 'MovieController'
         })
         .otherwise({
             redirectTo: '/'
