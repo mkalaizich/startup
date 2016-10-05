1b00ddd8666dae33af0b97afb6b18ce4cbadfa22
mkalaizich <mkalaizich@gmail.com>
2016-10-05 14:21:35 -0300

    [Martin Kalaizich] - Exercises 2 & 3 done - Keypoints 2 & 3 done. Still trying to improve code through component usage and routing

--- 03-MVC-AngularJS/script.js
+++ 03-MVC-AngularJS/script.js
@@ -1,25 +1,109 @@
-let aplic = angular.module('aplic', []);
+let app = angular.module('app', []);
 
-aplic.controller('moviesCtrl', function($scope, $http) {
+app.service('services', function(){
+
+    this.getIndex = function (title, target) {
+        for (let i = 0; i < target.length; i++){
+
+            if (target[i].title == title){
+
+                return index = i;
+                break;
+            }
+        }
+    }
+
+    this.store = function (newMovie) {
+
+        localStorage.setItem(newMovie.title, JSON.stringify({ title: newMovie.title, year: newMovie.year, plot: newMovie.plot }));
+    }
+
+    this.saveMovie = function (response) {
+
+        return newMovie = {
+                    title: response.data.Title,
+                    year: response.data.Year,
+                    plot: response.data.Plot,
+                };
+    }
+});
+
+app.controller('moviesCtrl', function($scope, $http, services) {
 
     let newMovie;
-    let myFavMovies = ['Gladiator',  'The Lion King', 'The Terminator'];
+    let myFavMovies = ['Gladiator', 'The Lion King', 'The Terminator'];
     
     $scope.movies = [];
     
-    for (let i = 0; i < myFavMovies.length ; i++) {
+    if (localStorage.length == 0) {
+        for (let i = 0; i < myFavMovies.length ; i++) {
             
-        let url = 'http://www.omdbapi.com/?t=' + myFavMovies[i].replace(/ /g, "+") + '&y=&plot=short&r=json';
+            let url = 'http://www.omdbapi.com/?t=' + myFavMovies[i].replace(/ /g, "+") + '&y=&plot=short&r=json';
 
-        $http.get(url).then(function(response) {
+            $http.get(url).then(function(response) {
                 
-            newMovie = {
-                title: response.data.Title,
-                year: response.data.Year,
-                plot: response.data.Plot,
-            };
-            $scope.movies.push({ title: newMovie.title, year: newMovie.year, plot: newMovie.plot });
-            localStorage.setItem(newMovie.title, JSON.stringify({ title: newMovie.title, year: newMovie.year, plot: newMovie.plot }));
-        });
+                newMovie = services.saveMovie(response);
+                $scope.movies.push({ title: newMovie.title, year: newMovie.year, plot: newMovie.plot });
+                services.store(newMovie);
+            });
+        }
+    } else {
+
+        for (let i = 0; i < localStorage.length; i++) {
+
+            let storedMovie = JSON.parse(localStorage.getItem(localStorage.key( i )));
+            $scope.movies.push({ title: storedMovie.title, year: storedMovie.year, plot: storedMovie.plot });
+        }
     }
-})
+});
+
+app.controller('changesCtrl', function($scope, $http, services) {
+  
+    $scope.add = function () {
+
+        if ($scope.title != ''){
+
+            let newMovie = {};
+            let url = 'http://www.omdbapi.com/?t=' + $scope.title.replace(/ /g, "+") + '&y=' + $scope.year + '&plot=short&r=json';
+            
+            $http.get(url).then(function(response) {
+                
+                if (response.data.Response == 'True') {
+                    
+                    newMovie = services.saveMovie(response);
+                    $scope.movies.push({ title: newMovie.title, year: newMovie.year, plot: newMovie.plot });
+                    services.store(newMovie);
+                }
+            });
+
+            $scope.title='';
+            $scope.year='';
+        }
+    }
+
+    $scope.remove = function () {
+
+        let index = -1;
+
+        index = services.getIndex($scope.toremove, $scope.movies);
+        
+        if(index >= 0) {
+
+            $scope.movies.splice(index, 1);
+            localStorage.removeItem($scope.toremove);
+        }
+
+        $scope.toremove = null;
+    }
+
+    $scope.editmovie = function () {
+
+        let index = -1;
+
+        index = services.getIndex($scope.toremove, $scope.movies);
+        $scope.movies[index].plot = $scope.editplot;
+        services.store($scope.movies[index]);
+        $scope.toremove = null;
+        $scope.editplot = '';
+    }
+});
